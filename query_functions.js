e5290a187a8755362a8621867f1078d79e0a300b
Sergey Zolotko <sergiozoloto@gmail.com>
2023-01-31 23:52:03 +0100

    query funcs updated

--- query_functions.js
+++ query_functions.js
@@ -3,27 +3,28 @@ export default function executeQuery(query, db, full_query = false) {
   db.query(query, (error, result) => {
     if (error) {
       // Formate error message conditionally
-      consoleErrorMessage(error, query, full_query);
+      logErrorMessage(error, query, full_query);
     } else {
       // Formate message conditionally
-      consoleQueryResult(result, query, full_query);
+      logQueryResult(result, query, full_query);
     }
   });
 }
 
 // Function rendering error message
-const consoleErrorMessage = (error, query, full_query) => {
-  let message = `ERROR. Query`;
+const logErrorMessage = (error, query, full_query) => {
+  let message = `ERROR. Query `;
   if (full_query) message = message + indentString(`\n${query}`, 2) + `\n`;
   message += `execution failed, returned message: `;
   message += `${error.sqlMessage}`;
   // Log the error message
-  console.error(`${message}\n\n`);
+  console.log(`${message}`);
+  if (full_query) console.log(`\n\n`);
 };
 
 // Function rendering query result
-const consoleQueryResult = (result, query, full_query) => {
-  let message = `Query`;
+const logQueryResult = (result, query, full_query) => {
+  let message = `Query `;
   if (full_query) message = message + indentString(`\n${query}`, 2) + `\n`;
   message += `executed.`;
   if (typeof result.affectedRows === 'number') {
@@ -35,9 +36,12 @@ const consoleQueryResult = (result, query, full_query) => {
     // Log the query status message
     console.log(message);
     // Log the query results
-    if (result.affectedRows == null) console.table(result);
+    if (result.affectedRows == null) {
+      console.table(result);
+      console.log(`\n`);
+    }
   }
-  console.log(`\n`);
+  if (full_query) console.log(`\n`);
 };
 
 const indentString = (string, count, indent = ' ') =>
