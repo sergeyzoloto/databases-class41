7c9cc7c10ff1d75c82ba6478a558fe8284fd00ff
Sergey Zolotko <sergiozoloto@gmail.com>
2023-02-10 13:54:51 +0100

    w03ex03 done

--- Week3/homework/Exercise3_SQL_injection.js
+++ Week3/homework/Exercise3_SQL_injection.js
@@ -10,7 +10,21 @@ const conn = mysql.createConnection({
 function getPopulation(Country, name, code, cb) {
   // assuming that connection to the database is established and stored as conn
   conn.query(
-    `SELECT Population FROM ${Country} WHERE Name = "${name}" and code = "${code}"`,
+    `SELECT Population FROM ${Country} WHERE Name = '${name}' and code = '${code}'`,
+    function (err, result) {
+      if (err) cb(err);
+      if (result.length == 0) cb(new Error('Not found'));
+      cb(result);
+    },
+  );
+}
+
+function getPopulationEscaped(Country, countryName, countryCode, cb) {
+  // assuming that connection to the database is established and stored as conn
+  const name = conn.escape(countryName);
+  const code = conn.escape(countryCode);
+  conn.query(
+    `SELECT Population FROM ${Country} WHERE Name = ${name} and code = ${code};`,
     function (err, result) {
       if (err) cb(err);
       if (result.length == 0) cb(new Error('Not found'));
@@ -20,5 +34,12 @@ function getPopulation(Country, name, code, cb) {
 }
 
 conn.connect();
-getPopulation('country', '" OR "1"="1', '" OR "1"="1', console.log);
+// Returns all the records in the database:
+getPopulation('country', "' OR '1'='1", "' OR '1'='1", console.log);
+
+// Returns error:
+getPopulationEscaped('country', "' OR '1'='1", "' OR '1'='1", console.log);
+
+// Works properly:
+getPopulationEscaped('country', 'Ukraine', 'UKR', console.log);
 conn.end();
